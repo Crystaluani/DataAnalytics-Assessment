8b31827b394f23e71a0c9f2d3a6ff86592fb2276
UANI <Necheuani@gmail.com>
2025-05-17 11:24:50 +0100

    Update Assessment_Q3.sql

--- Assessment_Q3.sql
+++ Assessment_Q3.sql
@@ -1,17 +1,24 @@
-USE adashi_staging;
+SELECT 
+  s.id AS plan_id,
+  s.owner_id,
+  'Savings' AS type,
+  MAX(s.created_on) AS last_transaction_date,
+  DATEDIFF(CURDATE(), MAX(s.created_on)) AS inactivity_days
+FROM savings_savingsaccount s
+WHERE s.confirmed_amount > 0
+GROUP BY s.id, s.owner_id
+HAVING inactivity_days > 365
 
-SELECT
-    u.id AS owner_id,
-    u.name,
-    MAX(s.created_on) AS last_activity
-FROM users_customuser u
-JOIN plans_plan p ON p.owner_id = u.id
-LEFT JOIN savings_savingsaccount s ON s.owner_id = u.id
-WHERE u.id IS NOT NULL
-GROUP BY u.id, u.name
-HAVING last_activity IS NULL
-   OR last_activity < DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
-ORDER BY last_activity
-LIMIT 50;
-SET GLOBAL net_read_timeout = 600;
-SET GLOBAL net_write_timeout = 600;
+UNION ALL
+
+SELECT 
+  p.id AS plan_id,
+  p.owner_id,
+  'Investment' AS type,
+  MAX(p.created_on) AS last_transaction_date,
+  DATEDIFF(CURDATE(), MAX(p.created_on)) AS inactivity_days
+FROM plans_plan p
+WHERE p.is_a_fund = 1 AND p.status_id = 1
+GROUP BY p.id, p.owner_id
+HAVING inactivity_days > 365
+ORDER BY inactivity_days DESC;
